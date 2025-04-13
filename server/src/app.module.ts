7726576d313d67a0a931b7d1068ae5f0ae47c178
Andri4nina <138116935+Andri4nina@users.noreply.github.com>
2025-04-13 20:49:18 +0300

    mini update

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AppGateway } from './App.gateway';
+import { AppGateway } from './app.gateway';
 
 
 @Module({
